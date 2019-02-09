b1140953e4ee73ceb6ddd2ded3cbf3b428038949
tts <tts@ymsht.info>
2019-02-09 22:02:06 +0900

    心拍数生成

--- create_heart_reate_minute.go
+++ create_heart_reate_minute.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/globalsign/mgo"
 )
@@ -15,14 +16,68 @@ func main() {
 
 	session, _ := mgo.Dial("mongodb://localhost")
 	defer session.Close()
+
 	db := session.DB("fitbit")
 
-	heartrate := Heartrate{60}
-	col := db.C("heart_rate_minute")
-	err := col.Insert(heartrate)
-	if err != nil {
-    	fmt.Println("Insert error:", err)
-	}
+	wg := sync.WaitGroup{}
+
+	// 0-9
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		var heartRate = 83
+		var num = 10 * 365 * 12 * 60
+		for i:=0; i < num; i++ {
+			heartrate := Heartrate{heartRate}
+			col := db.C("heart_rate_minute")
+			col.Insert(heartrate)
+		}
+
+		wg.Done()
+	}(&wg)
+
+	// 10-19
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		var heartRate = 70
+		var num = 10 * 365 * 12 * 60
+		for i:=0; i < num; i++ {
+			heartrate := Heartrate{heartRate}
+			col := db.C("heart_rate_minute")
+			col.Insert(heartrate)
+		}
+
+		wg.Done()
+	}(&wg)
+
+	// 20-29
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		var heartRate = 63
+		var num = 10 * 365 * 12 * 60
+		for i:=0; i < num; i++ {
+			heartrate := Heartrate{heartRate}
+			col := db.C("heart_rate_minute")
+			col.Insert(heartrate)
+		}
+
+		wg.Done()
+	}(&wg)
+
+	// 30-31(〜8月)
+	wg.Add(1)
+	go func(wg *sync.WaitGroup) {
+		var heartRate = 66
+		var num = 10 * 365 * 8 * 60
+		for i:=0; i < num; i++ {
+			heartrate := Heartrate{heartRate}
+			col := db.C("heart_rate_minute")
+			col.Insert(heartrate)
+		}
+
+		wg.Done()
+	}(&wg)
+
+	wg.Wait()
 
 	fmt.Println("End Batch.")
 }
